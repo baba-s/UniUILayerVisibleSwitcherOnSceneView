7ac65248658e4e46d9fd98eef6b66a05432093a0
baba_s <shotababa1206@gmail.com>
2020-08-16 09:26:30 +0900

    first commit

---
+++ Editor/UILayerVisibleSwitcherOnSceneView.cs
@@ -0,0 +1,40 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+	[InitializeOnLoad]
+	internal static class UILayerVisibleSwitcherOnSceneView
+	{
+		private const string BUTTON_TEXT = " UI ";
+
+		private static readonly Rect       m_buttonPosition = new Rect( 2, 2, 50, 20 );
+		private static readonly int        m_uiLayerMask    = LayerMask.GetMask( "UI" );
+		private static readonly GUIContent m_visibleContent = EditorGUIUtility.IconContent( "scenevis_visible_hover" );
+		private static readonly GUIContent m_hiddenContent  = EditorGUIUtility.IconContent( "scenevis_hidden_hover" );
+
+		static UILayerVisibleSwitcherOnSceneView()
+		{
+			m_visibleContent.text = BUTTON_TEXT;
+			m_hiddenContent.text  = BUTTON_TEXT;
+
+			SceneView.duringSceneGui += OnGUI;
+		}
+
+		private static void OnGUI( SceneView sceneView )
+		{
+			Handles.BeginGUI();
+
+			var isVisible = ( Tools.visibleLayers & m_uiLayerMask ) != 0;
+			var content   = isVisible ? m_visibleContent : m_hiddenContent;
+
+			if ( GUI.Button( m_buttonPosition, content ) )
+			{
+				Tools.visibleLayers ^= m_uiLayerMask;
+				SceneView.RepaintAll();
+			}
+
+			Handles.EndGUI();
+		}
+	}
+}
